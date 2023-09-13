a16f776a2bef37946a039ff7481b0109afde7ba7
Julia Powers <jepowers52@gmail.com>
2023-09-12 19:52:59 -0700

    calculations added

--- arithmetic.py
+++ arithmetic.py
@@ -4,32 +4,56 @@
 def add(num1, num2):
     """Return the sum of num1 and num2."""
 
-    return 10
+    add_num = num1 + num2
+
+    return add_num
 
 
 def subtract(num1, num2):
     """Return the value of num1 minus num2."""
 
+    subtract_num = num1 - num2
 
+    return subtract_num
+    
 def multiply(num1, num2):
     """Multiply the num1 by num2 and return the result."""
 
+    mult_num = num1 * num2
+
+    return mult_num
 
 def divide(num1, num2):
     """Divide the num1 by num2, returning a floating point."""
 
+    divide_num = num1 / num2
+
+    return divide_num
 
 def square(num1):
     """Return the square of num1."""
 
+    sq_num = num1 * num1
+
+    return sq_num
 
 def cube(num1):
     """Return the cube of num1."""
 
+    cube_num = num1 ** 3
+
+    return cube_num
 
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
 
+    pow_num = num1 ^ num2
+
+    return pow_num
 
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
+
+    modulo_num = num1 % num2
+
+    return modulo_num
