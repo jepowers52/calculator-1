9e9858b5e343da48ea02b3f55f84ff47bc89b3af
Julia Powers <jepowers52@gmail.com>
2023-09-12 19:56:02 -0700

    power function updated

--- arithmetic.py
+++ arithmetic.py
@@ -47,7 +47,7 @@ def cube(num1):
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
 
-    pow_num = num1 ^ num2
+    pow_num = num1 ** num2
 
     return pow_num
 
